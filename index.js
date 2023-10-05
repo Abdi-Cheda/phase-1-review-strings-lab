c501c8fb2db9fc0a78a767c2cd9760fd232cdba1
+ <abdicheda75@gmail.com>
2023-10-05 09:15:47 +0300

    Finished Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = "Abdi Cheda"
+const welcomeMessage = "Welcome to Flatbook, "+ currentUser + "!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+ currentUser.toUpperCase() + "!"
+console.log(excitedWelcomeMessage)
+const shortGreeting = "Welcome, " +currentUser[0]+"!"
